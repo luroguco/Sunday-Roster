0d12b7446828d6a930852c0c6848168e6c2abd6a
luroguco <luis.gutierrez@students.olin.edu>
2011-01-30 20:01:02 -0500

    rails webinar app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roster::Application.config.secret_token = '97b1773e294acb8ee1aaa9a1bb2d706f7a4686c00c3273955fc9f34c38f1ab974d07901340065f6a965e2a6b34b949d7ee087c553ef7ae1a13a9fa97b054cac8'
